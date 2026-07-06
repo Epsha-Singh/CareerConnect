9b182e3c1389b7f9fd22322f80e58848de47d2cd
Epsha-Singh <epsha.t2006@gmail.com>
2026-07-06 12:26:46 +0530

    Created Express Server

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.send("Welcome to CareerConnect API!");
+});
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
