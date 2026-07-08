0b5d695c2c698da94fc597b73f549afce7c04646
Epsha-Singh <epsha.t2006@gmail.com>
2026-07-08 12:06:58 +0530

    Complete CRUD API for jobs

--- server.js
+++ server.js
@@ -4,14 +4,7 @@ const app = express();
 
 app.use(express.json());
 
-// Home Route
-app.get("/", (req, res) => {
-    res.send("Welcome to CareerConnect API!");
-});
-
-// GET all jobs
-app.get("/api/jobs", (req, res) => {
-    const jobs = [
+const jobs = [
         {
             id: 1,
             title: "Frontend Developer",
@@ -35,7 +28,88 @@ app.get("/api/jobs", (req, res) => {
         }
     ];
 
+
+// Home Route
+app.get("/", (req, res) => {
+    res.send("Welcome to CareerConnect API!");
+});
+
+// GET all jobs
+app.get("/api/jobs", (req, res) => {
     res.json(jobs);
+});
+
+// GET a single job by ID
+app.get("/api/jobs/:id", (req, res) => {
+
+    const jobId = parseInt(req.params.id);
+
+    const job = jobs.find(job => job.id === jobId);
+
+    if (!job) {
+        return res.status(404).json({
+            message: "Job not found"
+        });
+    }
+
+    res.json(job);
+});
+
+// POST a new job
+app.post("/api/jobs", (req, res) => {
+
+    const newJob = {
+        id: jobs.length + 1,
+        title: req.body.title,
+        company: req.body.company,
+        location: req.body.location,
+        salary: req.body.salary
+    };
+
+    jobs.push(newJob);
+
+    res.status(201).json(newJob);
+});
+
+// UPDATE a job
+app.put("/api/jobs/:id", (req, res) => {
+
+    const jobId = parseInt(req.params.id);
+
+    const job = jobs.find(job => job.id === jobId);
+
+    if (!job) {
+        return res.status(404).json({
+            message: "Job not found"
+        });
+    }
+
+    job.title = req.body.title;
+    job.company = req.body.company;
+    job.location = req.body.location;
+    job.salary = req.body.salary;
+
+    res.json(job);
+});
+
+// DELETE a job
+app.delete("/api/jobs/:id", (req, res) => {
+
+    const jobId = parseInt(req.params.id);
+
+    const jobIndex = jobs.findIndex(job => job.id === jobId);
+
+    if (jobIndex === -1) {
+        return res.status(404).json({
+            message: "Job not found"
+        });
+    }
+
+    jobs.splice(jobIndex, 1);
+
+    res.json({
+        message: "Job deleted successfully"
+    });
 });
 
 const PORT = process.env.PORT || 5000;
